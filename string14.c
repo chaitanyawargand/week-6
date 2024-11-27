caf40903dcc2182b521280064a73074db88d0719
chaitanyawargand <chaitanyawargand@gmail.com>
2024-11-27 15:19:55 +0530

    Update string14.c

--- string14.c
+++ string14.c
@@ -1,28 +1,4 @@
 //14. Write a C program to cound the number of vowels, consonants, digits and white spaces in a string//
-/*#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int n,i,j,num=0,vo=0,co=0,space=0;
-    printf("enter the number of elements\n");
-    scan("%d",&n);
-    char ch[n+1];
-    int vo[10]={65,69,73,79,85,97,101,105,111,117};
-
-    fflush(stdin);
-    printf("enter the string\n");
-    scanf("%[^\n]s",ch);
-    for(i=0;i<n;i++)
-    {
-        if(ch[i]>0 && ch[i]<9)
-        num++;
-        for(j=0;j<10;j++)
-        {
-            if(ch[i]==vo[j])
-        }
-        
-    }
-}*/
 #include <stdio.h>
 
 int main() {
